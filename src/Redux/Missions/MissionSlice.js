1e058a04294190d8b32df7b9e6f4a32f4b5442ed
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-01 14:08:10 -0500

    Redux Action for join missions and leave mission

--- src/Redux/Missions/MissionSlice.js
+++ src/Redux/Missions/MissionSlice.js
@@ -11,27 +11,52 @@ export const getMissions = createAsyncThunk(
           mission_id: mission.mission_id,
           mission_name: mission.mission_name,
           description: mission.description,
+          reserved: false
         };
         return missions;
       }      
     )
     return missionApiInfo
-  }
-)
-
+      }
+    )
 const initialState = { loading: false, data: [], error: null };
 
 const missionsSlice = createSlice(
  {
      name: 'missions',
      initialState,
-     reducers: {},
+     reducers: {
+        joinMission(state, { payload }) {
+          const newMissions = [];
+          state.data.map((mission) => {
+            if (mission.mission_id === payload) {
+              newMissions.push({
+                ...mission,
+                reserved: !mission.reserved,
+              });
+            } else {
+              newMissions.push(mission);
+            }
+            return newMissions;
+          });
+          return { ...state, data: newMissions };
+        },
+      },
      extraReducers: (Builder) => {
         Builder.addCase(getMissions.fulfilled, (state, action) => {
             state.data = action.payload;
         })
+        .addCase(getMissions.pending, (state) => {
+            const newState = { ...state, loading: true };
+            return newState;
+          })
+        .addCase(getMissions.rejected, (state) => {
+            const newState = { ...state, error: 'Error 404. Failed to fetch' };
+            return newState;
+          });
      }
  }
 )
 
 export default missionsSlice.reducer
+export const { joinMission } = missionsSlice.actions;
--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,25 +1,34 @@
 import '../Styles/Missions.css';
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
-import { getMissions } from '../Redux/Missions/MissionSlice';
+import { getMissions, joinMission } from '../Redux/Missions/MissionSlice';
 
 
 const Missions = () => {
   const dispatch = useDispatch();
-  const state = true;
+ 
 
   const missions = useSelector((state) => state.Missions.data)
+
   useEffect(() => {
-    dispatch(getMissions());
-  });
+    if (missions.length === 0) {
+      dispatch(getMissions());
+    }
+  },);
 
-  const joinBtn = () => {
-   return state ? {display: 'none'} : {display: 'block'};
+
+  const handleJoin = async (id) => {
+     dispatch(joinMission(id))
   }
 
-  const leaveBtn = () => {
-    return state ? {display: 'block'} : {display: 'none'};
-  }
+  const joinBtn = (state) => {
+    return state === true ? {display: 'none'} : {display: 'block'};
+   }
+ 
+  const leaveBtn = (state) => {
+     return state === true ? {display: 'block'} : {display: 'none'};
+   }
+
 
   return (
     <div id="table-container">
@@ -34,14 +43,31 @@ const Missions = () => {
        </thead>
        <tbody>
          {missions.map((mission) => {
+
           return (
-          <tr key={mission.mission_id} id={mission.mission_id}>
-            <td className="mission"> { mission.mission_name} </td>
+          <tr key={mission.mission_id} >
+            <td className="mission"> { mission.mission_name} {mission.reserved.toString()} </td>
             <td className="description"> { mission.description } </td>
-            <td className="statusContainer"> <div className="status">NOT A MEMBER </div></td>
+            <td className="statusContainer"> 
+              <div className="joinStatus" style={joinBtn(mission.reserved)}>NOT A MEMBER </div>
+              <div className="activeStatus" style={leaveBtn(mission.reserved)}>Active Member</div>
+            </td>
             <td className="joinContainer"> 
-              <button className="joinBtn" style={joinBtn()}>Join Mission</button>
-              <button className="leaveBtn" style={leaveBtn()}>Leave Mission</button> 
+              <button 
+                className="joinBtn"   
+                id={mission.mission_id} 
+                onClick={ () => handleJoin(mission.mission_id, mission.reserved) }
+                style={joinBtn(mission.reserved)}
+                >
+                  Join Mission
+               </button>
+              <button 
+                className="leaveBtn"  
+                id={mission.mission_id} 
+                onClick={ () => handleJoin(mission.mission_id) }
+                style={leaveBtn(mission.reserved)}>
+                Leave Mission
+              </button> 
             </td>
           </tr>)
          })}
