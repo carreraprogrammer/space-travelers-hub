518a522013a337d595eac18d902b3b2c2639c0e4
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-30 19:26:51 -0500

    Add styles

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -6,21 +6,47 @@ import { getMissions } from '../Redux/Missions/MissionSlice';
 
 const Missions = () => {
   const dispatch = useDispatch();
+  const state = true;
 
   const missions = useSelector((state) => state.Missions.data)
   useEffect(() => {
     dispatch(getMissions());
   });
 
+  const joinBtn = () => {
+   return state ? {display: 'none'} : {display: 'block'};
+  }
+
+  const leaveBtn = () => {
+    return state ? {display: 'block'} : {display: 'none'};
+  }
+
   return (
-    <div className="missions-container">
-     {missions.map((mission) => {
-      return (
-      <div key={mission.mission_id} id={mission.mission_id}>
-        <h2> { mission.mission_name} </h2>
-        <p> { mission.description } </p>
-      </div>)
-     })}
+    <div id="table-container">
+      <table className="missions-table">
+       <thead>
+         <tr>
+           <th>Mission</th>
+           <th>Description</th>
+           <th>Status</th>
+           <th></th>
+         </tr>
+       </thead>
+       <tbody>
+         {missions.map((mission) => {
+          return (
+          <tr key={mission.mission_id} id={mission.mission_id}>
+            <td className="mission"> { mission.mission_name} </td>
+            <td className="description"> { mission.description } </td>
+            <td className="statusContainer"> <div className="status">NOT A MEMBER </div></td>
+            <td className="joinContainer"> 
+              <button className="joinBtn" style={joinBtn()}>Join Mission</button>
+              <button className="leaveBtn" style={leaveBtn()}>Leave Mission</button> 
+            </td>
+          </tr>)
+         })}
+       </tbody>
+      </table>
     </div>
   );
 };
