2b1c663f758fe5ef4545f153036349da6a8b170a
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-02 14:42:39 -0500

    Display reserved rockets in my profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -5,25 +5,49 @@ const MyProfile = () => {
 
   const filteredMissions = useSelector((state)=> state.Missions.myMissions)
 
+  const filteredRockets = useSelector((state) => state.Rockets.rockets);
+
   return (
-   <div id="missionsContainer">
-    <table id="myMissions">
-      <thead>
-        <tr>
-           <th>My Missions</th> 
-        </tr>
-      </thead>
-      <tbody>
-        
-      {filteredMissions.map((mission) => {
-        return (
-            <tr key={mission.mission_id} >
-               <td className="myMissionInfo">{mission.mission_name}</td> 
+   <div id="profileContainer">
+     <div id="missionsContainer">
+      <table id="myMissions">
+        <thead>
+          <tr>
+             <th>My Missions</th> 
+          </tr>
+        </thead>
+        <tbody>
+          
+        {filteredMissions.map((mission) => {
+          return (
+              <tr key={mission.mission_id} >
+                 <td className="myMissionInfo">{mission.mission_name}</td> 
+              </tr>
+          )   
+       })}
+        </tbody>
+      </table>
+      </div>
+        <div id="rocketsContainer">
+        <table id="myRockets">
+          <thead>
+            <tr>
+              <th>My Rockets</th>
             </tr>
-        )   
-     })}
-      </tbody>
-    </table>
+          </thead>
+          <tbody>
+            {filteredRockets.map((rocket) => {
+              if (rocket.reserved === true)
+                return (
+                  <tr key={rocket.rocket_id + "rockets"}>
+                    <td>{rocket.rocketName}</td>
+                  </tr>
+                );
+              return null;
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
