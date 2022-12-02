2eb6dbb11102d120e7a087ed519cc43c6d98fcc8
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-01 19:22:54 -0500

    Update missions style

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -6,13 +6,25 @@ const MyProfile = () => {
   const filteredMissions = useSelector((state)=> state.Missions.myMissions)
 
   return (
-    <>
+   <div id="missionsContainer">
+    <table id="myMissions">
+      <thead>
+        <tr>
+           <th>My Missions</th> 
+        </tr>
+      </thead>
+      <tbody>
+        
       {filteredMissions.map((mission) => {
-        return ( 
-          <h1 key={mission.mission_id}>{mission.mission_name}</h1>
-        )
+        return (
+            <tr key={mission.mission_id} >
+               <td className="myMissionInfo">{mission.mission_name}</td> 
+            </tr>
+        )   
      })}
-    </>
+      </tbody>
+    </table>
+    </div>
   )
 }
 
