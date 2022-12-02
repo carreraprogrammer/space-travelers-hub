6e752340bb0091868f684ea3e933fbe13d94082a
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-02 14:20:27 -0500

    Add rockets component

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,8 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { getRockets, joinMission } from '../components/Redux/RocketsRedux';
+
 const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.Rockets.rockets);
+
+  useEffect(() => {
+    if (rockets.length === 0) dispatch(getRockets());
+  });
+
+  const handleJoin = async (id) => {
+    dispatch(joinMission(id));
+  };
+
   return (
-    <div ClassName="stat-container">
-      Check Status
-      <p>test</p>
+    <div className="centered">
+      {rockets.map((rocket) => (
+        <div className="info" key={rocket.rocket_id}>
+          <img className="rocket-img" src={rocket.flickrImages} alt="" />
+          <div className="center">
+            <h2>{rocket.rocketName}</h2>
+            <p>{rocket.reserved && <span class="span">reserved</span>}</p>
+            <h2>{rocket.type}</h2>
+            <p>{rocket.description}</p>
+
+            <button
+              type="button"
+              id={rocket.rocket_id}
+              onClick={() => handleJoin(rocket.rocket_id, rocket.reserved)}
+              className={!rocket.reserved ? 'reserve-btn ' : 'cancel-btn'}
+            >
+              {!rocket.reserved ? 'Reserve Rocket' : 'Cancel Reservation'}
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
